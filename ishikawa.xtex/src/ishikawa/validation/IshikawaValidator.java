c976d5b4a772774f6c23673eaf742515b171d974
BMGKS73\Administrador <bss731@alumnos.unican.es>
2020-12-28 12:37:23 +0100

    Se comienza con la validación. En mwe2 se añade una línea que permite asegurar que los nombres de las entidades principales son únicos. Los nombres de las categorías serán únicos, no así los de las causas.
    Por otro lado, se añaden en IshikawaValidator más normas de validación
    que levantan un error si los nombres de Efecto/Categoría/Causa no
    existen o si son vacíos.

---
+++ ishikawa.xtex/src/ishikawa/validation/IshikawaValidator.java
@@ -0,0 +1,69 @@
+package ishikawa.validation;
+
+import org.eclipse.xtext.validation.Check;
+
+import ishikawa_model.*;
+
+/**
+ * This class contains custom validation rules. 
+ *
+ * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#validation
+ */
+public class IshikawaValidator extends AbstractIshikawaValidator {
+	
+	public static final String INVALID_NAME = "invalidName";
+
+	@Check
+	public void checkEffectNameNotBlank(Effect effect) {
+		if (effect.getName().isBlank()) {
+			error("Effect's name must not be blank",
+					Ishikawa_modelPackage.Literals.EFFECT__NAME,
+					INVALID_NAME);
+		}
+	}
+	
+	@Check
+	public void checkCategoryNameNotBlank(Category category) {
+		if (category.getName().isBlank()) {
+			error("Category's name must not be blank",
+					Ishikawa_modelPackage.Literals.CATEGORY__NAME,
+					INVALID_NAME);
+		}
+	}
+	
+	@Check
+	public void checkCauseNameNotBlank(Cause cause) {
+		if (cause.getName().isBlank()) {
+			error("Cause's name must not be blank",
+					Ishikawa_modelPackage.Literals.CAUSE__NAME,
+					INVALID_NAME);
+		}
+	}
+	
+	@Check
+	public void checkEffectNameExists(Effect effect) {
+		if (effect.getName()==null) {
+			error("Effect's name must exist",
+					Ishikawa_modelPackage.Literals.EFFECT__NAME,
+					INVALID_NAME);
+		}
+	}
+	
+	@Check
+	public void checkCategoryNameExists(Category category) {
+		if (category.getName()==null) {
+			error("Category's name must exist",
+					Ishikawa_modelPackage.Literals.CATEGORY__NAME,
+					INVALID_NAME);
+		}
+	}
+
+	@Check
+	public void checkCauseNameExists(Cause cause) {
+		if (cause.getName()==null) {
+			error("Cause's name must exist",
+					Ishikawa_modelPackage.Literals.CAUSE__NAME,
+					INVALID_NAME);
+		}
+	}
+}
